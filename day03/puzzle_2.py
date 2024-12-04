058bea4b91fd495128c3ac479212fe366e4d9ce7
patricknormile <patnormil@gmail.com>
2024-12-04 11:20:23 -0700

    update puzzle 2

--- day03/puzzle_2.py
+++ day03/puzzle_2.py
@@ -7,43 +7,34 @@ sys.path.append(str(root))
 from utils.get_data import get_days_data
 from day03.puzzle_1 import find_matches, get_values_to_multiply
 
-def find_donts(data):
-    regex = r"don't\(\)"
-    matches = re.finditer(regex, data, re.MULTILINE)
-    for match in matches:
-        yield match.span()[1]
-
-def find_dos(data):
-    regex = r"do\(\)"
-    matches = re.finditer(regex, data, re.MULTILINE)
-    for match in matches:
-        yield match.span()[0]
-
-def off_limits(match, data):
-    use = True
-    start, stop = match.span()[0], match.span()[1]
-    donts = find_donts(data)
-    dos = find_dos(data)
-    for dont in donts:
-        if start > dont:
-            for do in dos:
-                if do < dont:
-                    continue
-                if stop < do and do > dont:
-                    use = False
-                    break
-                if stop > do:
-                    break
-    return use
+def scan_for_matches(data):
+    """
+    very shit loop through each char
+    """
+    status = True
+    regex = r"mul\(\d{1,3},\d{1,3}\)$"
+    stop_regex = r"don't\(\)$"
+    start_regex = r"do\(\)$"
+    numbers = []
+    n = len(data)
+    for i in range(12,n):
+        substring = data[i-12:i]
+        if status:
+            if re.search(regex, substring) is not None:
+                match = re.search(regex, substring).string
+                numbers.append(get_values_to_multiply(match))
+            elif re.search(stop_regex, substring):
+                status = False
+        else:
+            if re.search(start_regex, substring):
+                status = True
+    output = sum([x[0] * x[1] for x in numbers])
+    return output
 
 
 def main():
     data = get_days_data(2024, 3)
-    matches = find_matches(data)
-    values = [get_values_to_multiply(x.group()) for x in matches 
-        if off_limits(x, data)]
-    output = sum([x[0] * x[1] for x in values])
-    print(output)
+    print(scan_for_matches(data))
 
 if __name__ == "__main__":
     main()
